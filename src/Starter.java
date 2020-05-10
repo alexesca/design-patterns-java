dea916078d557c7e6624c456c6ec3a84f5bbc281
alexesca <alexescamore@gmail.com>
2020-05-09 21:31:21 -0500

    Saved changes

--- src/Starter.java
+++ src/Starter.java
@@ -7,13 +7,10 @@ import Decorator.Window;
 public class Starter {
 
 	public static void main(String[] args) {
-//		Window window = new Window();
-//		IconWindowDecorator iconWindowDecorator = new IconWindowDecorator(window);
-//		ScrollBarWindowDecorator scrollBarWindowDecorator = new ScrollBarWindowDecorator(iconWindowDecorator);
-//		scrollBarWindowDecorator.draw();
-		
-		
-new TextEditor().main();
+		Window window = new Window();
+		IconWindowDecorator iconWindowDecorator = new IconWindowDecorator(window);
+		ScrollBarWindowDecorator scrollBarWindowDecorator = new ScrollBarWindowDecorator(iconWindowDecorator);
+		scrollBarWindowDecorator.draw();
 	}
 
 }
